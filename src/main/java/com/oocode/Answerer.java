bee4620d2d85788890592717cc735de3c9a4820f
Jean-Marie Bise <goupilapps@gmail.com>
2024-04-25 15:12:45 +0100

    Largest

--- src/main/java/com/oocode/Answerer.java
+++ src/main/java/com/oocode/Answerer.java
@@ -1,7 +1,20 @@
 package com.oocode;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class Answerer {
     public String answerFor(String question) {
-        return "dfiner";
+        if (question.equals("What is your name?")) return "dfiner";
+
+        if (question.startsWith("Which of the following numbers is the largest:")) {
+            return largest(question);
+        }
+        return "??";
+
+    }
+
+    private String largest(String q){
+        return Arrays.stream(q.split(":")[1].split(",")).map(String::trim).max(Comparator.comparingInt(value -> -Integer.parseInt(value))).get();
     }
 }
--- src/test/java/com/oocode/AnswererTest.java
+++ src/test/java/com/oocode/AnswererTest.java
@@ -1,5 +1,6 @@
 package com.oocode;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.CoreMatchers.equalTo;
